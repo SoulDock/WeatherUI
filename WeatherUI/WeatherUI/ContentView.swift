f884447d02fce6906e2bacf151555b3279b3b9da
Stanislaw <souldocker@gmail.com>
2023-03-15 19:47:09 +0300

    Background gradient and Title text

--- WeatherUI/WeatherUI/ContentView.swift
+++ WeatherUI/WeatherUI/ContentView.swift
@@ -1,21 +1,18 @@
-//
-//  ContentView.swift
-//  WeatherUI
-//
-//  Created by Стас Асташенко on 15.03.2023.
-//
-
 import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        ZStack{
+            LinearGradient(gradient: Gradient(colors: [.blue, .white]), startPoint: .topLeading, endPoint: .bottomTrailing)
+                .edgesIgnoringSafeArea(.all)
+            VStack{
+                Text("London, GB")
+                    .font(.system(size: 32,weight: .medium, design: .default))
+                    .foregroundColor(.white)
+                    .padding()
+                Spacer()
+            }
         }
-        .padding()
     }
 }
 
