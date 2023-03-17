22aa09681b9d4cdcec7232c11b695a8c7e7f5151
Stanislaw <souldocker@gmail.com>
2023-03-17 15:07:49 +0300

    BackgroundView Binding refactor

--- WeatherUI/WeatherUI/ContentView.swift
+++ WeatherUI/WeatherUI/ContentView.swift
@@ -4,11 +4,10 @@ struct ContentView: View {
     @State private var isNight = false
     var body: some View {
         ZStack{
-            BackgroundView(topColor: isNight ? .black : .blue,
-                           bottomColor: isNight ? .gray : Color("lightBlue"))
+            BackgroundView(isNight: $isNight)
             VStack{
                 SityTextViev(sityName: "London, GB")
-                MainWeatherView(imageName: "cloud.sun.fill", temperature: 18)
+                MainWeatherView(imageName: isNight ? "moon.stars.fill" : "cloud.sun.fill", temperature: isNight ? 13 : 18)
                 HStack(spacing: 20){
                     WeatherDayView(dayOfWeek: "MON",
                                    imageName: "cloud.sun.fill",
@@ -68,10 +67,11 @@ struct WeatherDayView: View {
 }
 
 struct BackgroundView: View {
-    var topColor: Color
-    var bottomColor: Color
+    
+    @Binding var isNight: Bool
+    
     var body: some View {
-        LinearGradient(gradient: Gradient(colors: [topColor, bottomColor]), startPoint: .topLeading, endPoint: .bottomTrailing)
+        LinearGradient(gradient: Gradient(colors: [ isNight ? .black : .blue, isNight ? .white : Color("lightBlue")]), startPoint: .topLeading, endPoint: .bottomTrailing)
             .edgesIgnoringSafeArea(.all)
     }
 }
